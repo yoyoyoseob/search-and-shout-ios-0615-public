e5986437766bd65b3d35efe21db1277f5ebf75e2
zdrossman <zachary.drossman@gmail.com>
2014-10-22 09:54:42 -0400

    Cleanup updates

---
+++ Pods/Target Support Files/Pods-searchAndShoutTests-OCHamcrest/Pods-searchAndShoutTests-OCHamcrest-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_searchAndShoutTests_OCHamcrest : NSObject
+@end
+@implementation PodsDummy_Pods_searchAndShoutTests_OCHamcrest
+@end
---
+++ Pods/Target Support Files/Pods-searchAndShoutTests-Specta/Pods-searchAndShoutTests-Specta-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_searchAndShoutTests_Specta : NSObject
+@end
+@implementation PodsDummy_Pods_searchAndShoutTests_Specta
+@end
